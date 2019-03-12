a08c6999da4336b6b300877ec48c345a1e3345b7
kduong@stuy.edu <kduong@stuy.edu>
2019-03-11 22:35:19 -0400

    finished add method, toString, constructor, size method

--- List_inArraySlots.java
+++ List_inArraySlots.java
@@ -1,10 +1,16 @@
+/**
+  Implement a list of diverse types, including
+  integers, double-precision floating point numbers,
+  and Strings.
+ */
+
 public class List_inArraySlots {
 
     private int[]    intElements;
     private double[] doubleElements;
     private String[] stringElements;
     private int filledElements; // the number of elements in this list
-    
+
     /* type identifier for each element
        That is, typeOfElements[i] == 0 means element i is an integer;
                                      1 means element i is a double;
@@ -20,34 +26,64 @@ public class List_inArraySlots {
       Construct an empty list with a small initial capacity.
      */
     public List_inArraySlots() {
+      intElements = new int[INITIAL_CAPACITY];
+      doubleElements = new double[INITIAL_CAPACITY];
+      stringElements = new String[INITIAL_CAPACITY];
+      typeOfElements = new int[INITIAL_CAPACITY];
     }
 
 
     /**
       @return the number of elements in this list
      */
-    // public int size() {
-    // }
+    public int size() {
+      return filledElements;
+    }
 
 
      /**
        @return a string representation of this list,
        in [a,b,c,] format
       */
-    // public String toString() {
-    // }
+      public String toString() {
+        String s = "[";
+        for (int index = 0; index < filledElements; index++ ) {
+          if (typeOfElements[index] == 0) {
+            s+= intElements[index] + ",";
+          } else {
+            if (typeOfElements[index] == 1) {
+              s+= doubleElements[index] + ",";
+            } else {
+              s+= stringElements[index] + ",";
+            }
+          }
+        }
+        return s+= "]";
+      }
 
 
     /**
       Appends @value to the end of this list.
       @return true, in keeping with conventions yet to be discussed
      */
-     // public boolean add( int type   // same meaning as in typeOfElements
-                       // , int intValue
-                       // , double doubleValue
-                       // , String stringValue
-                       // ) {
-     // }
+     public boolean add( int type   // same meaning as in typeOfElements
+                              , int intValue
+                              , double doubleValue
+                              , String stringValue
+                              ) {
+       if (type == 0) {
+         intElements[filledElements] = intValue;
+       } else {
+         if (type == 1) {
+           doubleElements[filledElements] = doubleValue;
+         } else {
+           stringElements[filledElements] = stringValue;
+         }
+       }
+       typeOfElements[filledElements] = type;
+       filledElements++;
+       return true;
+     }
 
 
     /**
